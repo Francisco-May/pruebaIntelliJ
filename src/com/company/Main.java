48bf7af5eeeded928509b440b63f88cb14283d97
Francisco May <francsicomiguelmaysalazar@outlook.com>
2021-04-28 01:08:36 -0500

    repaso java ciclo y demas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 import com.company.model.Product;
 
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -39,20 +40,48 @@ public class Main {
         }
         System.out.println(nombredia);
 
+        try {
+            Scanner util =new Scanner(System.in);
+            System.out.println("Articulo: ");
+            String articulo = util.nextLine(); //sirve para capturar datos String
+            System.out.println("Cantidad: ");
+            int canti = util.nextInt(); //sirve para capturar datos de tipo Int
+            System.out.println("Precio: ");
+            double precio = util.nextDouble(); //sirve para capturar datos tipo Double
 
-        Scanner util =new Scanner(System.in);
-        System.out.println("Articulo: ");
-        String articulo = util.nextLine();
-        System.out.println("Cantidad: ");
-        int canti = util.nextInt();
-        System.out.println("Precio: ");
-        double precio = util.nextDouble();
+            System.out.println("\n\n--Factura---");
+            System.out.println("Cantidad: "+canti);
+            System.out.println("Articulo: "+articulo);
+            System.out.println("Precio: "+precio);
+            System.out.println("Importe: "+(canti*precio));
+        }
+        catch (InputMismatchException error1){
+            System.out.println("A ocurrido un error de tipo: "+error1.toString());
+        }
+        catch (Exception error2){
+            System.out.println("A ocurrido un error de tipo: "+error2.toString());
+        }
+        finally {
+            //normalmente se utiliza para limpiar, pero como en este sistema no se usa tanta memoria
+            //pues no fue necesaria usar un metodo tan grande, para fines didacticos solo imprimiremos texto
+            System.out.println("Gracias por usar nuestro sistema");
+        }
 
-        System.out.println("\n\n--Factura---");
-        System.out.println("Cantidad: "+canti);
-        System.out.println("Articulo: "+articulo);
-        System.out.println("Precio: "+precio);
-        System.out.println("Importe: "+(canti*precio));
+        //ciclo for
+        for (int x = 0; x <= 10; x++){ //si qerer ir de 2 en 2 la expresion final seria x+=2
+            System.out.println(x);
+        }
+        //para ejemplo de foreach usamos vectores
+        char[] vector = new char[6];
+        vector[0]='C';
+        vector[1]='h';
+        vector[2]='r';
+        vector[3]='o';
+        vector[4]='n';
+        vector[5]='o';
 
+        for (char letra: vector){
+            System.out.print(letra);
+        }
     }
 }
